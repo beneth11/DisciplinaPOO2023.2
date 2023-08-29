04bf66a6fdf5418bd99fd35f41cb3bb5be27762b
beneth11 <141766881+beneth11@users.noreply.github.com>
2023-08-29 03:31:48 -0300

    Add files via upload

---
+++ Questões/Q08R/src/principal/edu/br/Principal.java
@@ -0,0 +1,38 @@
+package principal.edu.br;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Coloque o valor do depósito: ");
+		double dep= scan.nextDouble();
+		
+		
+		System.out.println("Coloque o valor da taxa: ");
+		double taxa= scan.nextDouble();
+		
+		
+		double rend = dep * taxa/100;
+		
+		
+		double totalrenda= dep + rend;
+		
+		
+		System.out.println("A sua renda é de: " +rend);
+		
+		
+		System.out.println("A sua renda total é de: " +totalrenda);
+		
+		
+		
+	
+		
+		
+		
+
+	}
+
+}
