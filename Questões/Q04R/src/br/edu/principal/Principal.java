21c224a3c0d09ca746b1c11a04f17fd23574e10d
beneth11 <141766881+beneth11@users.noreply.github.com>
2023-09-12 14:05:52 -0300

    Add files via upload

---
+++ Questões/Q04R/src/br/edu/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int Salario = 2000;
+		System.out.println("O seu salário anterior era de: " + Salario + ".");
+		int Salario2 = Salario + Salario * 25/100; 
+		System.out.println("Agora ele é de: " + Salario2 + ". Parabéns.");
+
+	}
+
+}
