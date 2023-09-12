95d93e23f750b5949d46cea09c219e5c18dbbb67
beneth11 <141766881+beneth11@users.noreply.github.com>
2023-09-12 14:53:11 -0300

    Add files via upload

---
+++ Questões/Q11R/src/br/edu/principal/Principal.java
@@ -0,0 +1,29 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int num = 2;
+		
+		double raizQ = Math.sqrt(num);
+		
+		double raizC = Math.cbrt(num);
+		
+		double quadnum = 2^2;
+		
+		double cubnum = 2^2;
+		
+		
+		System.out.println("O número é: " + num);
+		
+		System.out.println("A raíz quadrada é de: " + quadnum);
+		
+		System.out.println("A raíz cúbica é de: " + cubnum);
+		
+		System.out.println("O valor do número ao quadrado é " + raizQ);
+		
+		System.out.println("O valor do número ao cubo é: " + raizC);
+		
+	}
+
+}
